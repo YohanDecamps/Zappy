98d1c6eede572502d462cb967cf86726fd8410c1
Clément <clement2104.boillot@gmail.com>
2024-06-22 06:42:33 +0200

    feat: change queue size

--- server/src/ai_clients/commmands/inventory.c
+++ server/src/ai_clients/commmands/inventory.c
@@ -11,16 +11,11 @@
 void ai_cmd_inventory(
     UNUSED server_t *server, ai_client_t *client, UNUSED char *args)
 {
-    bool a = false;
-
     ai_dprintf(client, "[");
     for (int i = FOOD; i < THYSTAME; ++i) {
-        if (client->res[i].quantity == 0)
-            continue;
         ai_dprintf(
-            client, "%s%s %d", a ? ", " : "", r_name[i],
+            client, "%s%s %d", i ? ", " : "", r_name[i],
             client->res[i].quantity);
-        a = true;
     }
     ai_dprintf(client, "]\n");
 }
